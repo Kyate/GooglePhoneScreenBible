68df1c7586a280a519fcb7202fc0dd9018dab8bd
Jerome <jjlzyz000@gmail.com>
2016-06-13 23:39:05 -0400

    string decompression

---
+++ GooglePhoneBible/StringDecompression.h
@@ -0,0 +1,61 @@
+//
+//  StringDecompression.h
+//  GooglePhoneBible
+//
+//  Created by Jerome on 6/13/16.
+//  Copyright © 2016 Jerome. All rights reserved.
+//
+
+#ifndef StringDecompression_h
+#define StringDecompression_h
+
+#include "DataStructure.h"
+
+using namespace std;
+
+string decomHelper(string & s, int & pos) {
+    string current;
+    int repNum = 0;
+    while(pos < s.length() && s[pos]!=']') {
+        
+        while(pos < s.length() && (s[pos]<'0' || s[pos]>'9') && s[pos]!=']') {
+            current.push_back(s[pos]);
+            pos++;
+        }
+        
+        if (pos<s.length() && s[pos]==']') {pos++; return current;}
+        if (pos<s.length() && s[pos]>='0' && s[pos]<='9') {
+            int startpos = pos;
+            while(pos<s.length() && s[pos]>='0' && s[pos]<='9') pos++;
+            string numStr = s.substr(startpos, pos-startpos);
+            repNum = stoi(numStr);
+            pos++;
+            string next = decomHelper(s, pos);
+            for (int i = 0; i < repNum; i++) current+=next;
+        }
+    }
+    
+    if (pos<s.length() && s[pos]==']') pos++;
+    
+    return current;
+}
+
+string StringDecompression(string & s) {
+    string result;
+    int pos = 0;
+    while(pos<s.length()){
+        if (s[pos]<'0' || s[pos]>'9') {
+            result.push_back(s[pos]);
+            pos++;
+        }
+        else {
+            result += decomHelper(s, pos);
+        }
+    }
+    
+    return result;
+}
+
+
+
+#endif /* StringDecompression_h */
--- GooglePhoneBible/main.cpp
+++ GooglePhoneBible/main.cpp
@@ -12,35 +12,18 @@
 #include <mutex>
 #include <thread>
 
-#include "MovingAverageDataStream.h"
+#include "StringDecompression.h"
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     
     
-    MovingAverage ma(3);
-    std::thread t1(&MovingAverage::next, &ma, 1);
-    t1.join();
-    std::thread t2(&MovingAverage::next, &ma, 3);
-    t2.join();
-    std::thread t3(&MovingAverage::next, &ma, 4);
-    std::thread t4(&MovingAverage::next, &ma, 0);
-    std::thread t5(&MovingAverage::next, &ma, 10);
-    std::thread t6(&MovingAverage::next, &ma, 2);
-
+    string s = "2[ab3[d]uo]2[cc]";
     
+    string result = StringDecompression(s);
     
-    t3.join();
-    t4.join();
-    t5.join();
-    t6.join();
-    
-    //    std::cout << ma.next(1) << std::endl;
-//    std::cout << ma.next(3)<< std::endl;
-//    std::cout << ma.next(7)<< std::endl;
-//    std::cout << ma.next(2)<< std::endl;
-//    std::cout << ma.next(6)<< std::endl;
+    cout << result << std::endl;
 
 
     return 0;
